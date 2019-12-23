8f59f78ac9466d21520128fca74ce9a04ee08207
wangqifan <wangqifan@wangqifandeMacBook-Pro.local>
2019-12-23 13:26:58 +0800

    edit test

--- test.c
+++ test.c
@@ -5,10 +5,7 @@ int main() {
     app_msgx("test log app starting");
     app_debugx("test log app starting");
     struct acceptor *acceptor  = acceptor_init(80);
-    char str[80];
-    sprintf(str, "listen fd is:   %d", acceptor->listen_fd);
-    app_msgx(str);
-    sprintf(str, "listen port is:  %d", acceptor->listen_port);
-    app_msgx(str);
+    app_msgx("listen fd is:   %d", acceptor->listen_fd);
+    app_msgx("listen port is:  %d", acceptor->listen_port);
     return 0;
 }
