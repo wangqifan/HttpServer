bef20c851cdf6a08a24ff7752d56adafd4e47b8b
wangqifan <wangqifan@wangqifandeMacBook-Pro.local>
2020-03-10 15:04:59 +0800

    edit code

--- lib/tcp_server.c
+++ lib/tcp_server.c
@@ -5,42 +5,6 @@
 #include "thread_pool.h"
 #include "tcp_connection.h"
 
-int tcp_server(int port) {
-    int listenfd;
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    struct sockaddr_in server_addr;
-    bzero(&server_addr, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(port);
-
-    int on = 1;
-    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
-
-    int rt1 = bind(listenfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
-    if (rt1 < 0) {
-        error(1, errno, "bind failed ");
-    }
-
-    int rt2 = listen(listenfd, LISTENQ);
-    if (rt2 < 0) {
-        error(1, errno, "listen failed ");
-    }
-
-    signal(SIGPIPE, SIG_IGN);
-
-    int connfd;
-    struct sockaddr_in client_addr;
-    socklen_t client_len = sizeof(client_addr);
-
-    if ((connfd = accept(listenfd, (struct sockaddr *) &client_addr, &client_len)) < 0) {
-        error(1, errno, "bind failed ");
-    }
-
-    return connfd;
-}
-
 struct TCPserver *
 tcp_server_init(struct event_loop *eventLoop, struct acceptor *acceptor,
                 connection_completed_call_back connectionCompletedCallBack,
